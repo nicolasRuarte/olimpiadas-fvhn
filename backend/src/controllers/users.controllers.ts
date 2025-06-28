7211f350d2cc377b3ba5b1370130530dd0542691
nicolasRuarte <miqueasnruarte@protonmail.com>
2025-06-28 17:43:45 -0300

    Agregando ejemplo más completo de lo que quiero que hagan

--- backend/src/controllers/users.controllers.ts
+++ backend/src/controllers/users.controllers.ts
@@ -1,5 +1,17 @@
 import { Request, Response } from "express";
 
 export function createUser(req: Request, res: Response) {
-    res.send("Creando usuario");
+    res.send("Usuario creado");
+}
+
+export function getUser(req: Request, res: Response) {
+    res.send("Usuario devuelto");
+}
+
+export function updateUser(req: Request, res: Response) {
+    res.send("Usuario actualizado");
+}
+
+export function deleteUser(req: Request, res: Response) {
+    res.send("Usuario borrado");
 }
--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
-import { createUser } from "../controllers/users.controllers";
+import { createUser, getUser, updateUser, deleteUser } from "../controllers/users.controllers";
 
 const router = Router()
 
 router.post("/users", createUser);
+router.get("/users", getUser);
+router.put("/users", updateUser);
+router.delete("/users", deleteUser);
 
 export default router
