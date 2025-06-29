6cbf52957a2cc72e0b993b233e74da79ee88db14
nicolasRuarte <miqueasnruarte@protonmail.com>
2025-06-29 02:40:19 -0300

    Agregando cambios compilados

---
+++ backend/dist/routes/user.routes.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const users_controllers_1 = require("../controllers/users.controllers");
+const router = (0, express_1.Router)();
+router.post("/users", users_controllers_1.createUser);
+router.get("/users", users_controllers_1.getUser);
+router.put("/users", users_controllers_1.updateUser);
+router.delete("/users", users_controllers_1.deleteUser);
+exports.default = router;
