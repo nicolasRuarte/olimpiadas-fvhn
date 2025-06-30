59e02f9ad968a5954e8c748c7b0a23c90f848ae3
nicolasRuarte <miqueasnruarte@protonmail.com>
2025-06-29 21:58:49 -0300

    chore: cambiando URL a minúscula

--- backend/src/controllers/order.controllers.ts
+++ backend/src/controllers/order.controllers.ts
@@ -1,17 +1,17 @@
 import { Request, Response } from "express";
 
-export function createOrder(req: Request, res: Response) {
+export async function createOrder(req: Request, res: Response) {
     res.send("Pedido creado");
 }
 
-export function getOrder(req: Request, res: Response) {
+export async function readOrder(req: Request, res: Response) {
     res.send("Pedido devuelto");
 }
 
-export function updateOrder(req: Request, res: Response) {
+export async function updateOrder(req: Request, res: Response) {
     res.send("Pedido actualizado");
 }
 
-export function deleteOrder(req: Request, res: Response) {
+export async function deleteOrder(req: Request, res: Response) {
     res.send("Pedido borrado");
 }
--- backend/src/routes/order.routes.ts
+++ backend/src/routes/order.routes.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { createOrder, getOrder, updateOrder, deleteOrder } from "../controllers/order.controllers";
+import { createOrder, readOrder, updateOrder, deleteOrder } from "../controllers/order.controllers";
 
 const router = Router()
 
-router.post("/Order", createOrder);
-router.get("/Order", getOrder);
-router.put("/Order", updateOrder);
-router.delete("/Order", deleteOrder);
+router.post("/order", createOrder);
+router.get("/order", readOrder);
+router.put("/order", updateOrder);
+router.delete("/order", deleteOrder);
 
 export default router
