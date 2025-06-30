05d0c94b26641ad80dc4b0fbdb9a1b2c09c6a7b5
nicolasRuarte <miqueasnruarte@protonmail.com>
2025-06-30 13:52:37 -0300

    chore: cambiando nombres de funciones para seguir la conveción

--- backend/src/routes/orderdetail.routes.ts
+++ backend/src/routes/orderdetail.routes.ts
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { createOrderDetail, getOrderDetail, updateOrderDetail, deleteOrderDetail } from "../controllers/orderdetail.controllers";
+import { createOrderDetail, readOrderDetail, updateOrderDetail, deleteOrderDetail } from "../controllers/orderdetail.controllers";
 
 const router = Router()
 
 router.post("/OrderDetail", createOrderDetail);
-router.get("/OrderDetail", getOrderDetail);
+router.get("/OrderDetail", readOrderDetail);
 router.put("/OrderDetail", updateOrderDetail);
 router.delete("/OrderDetail", deleteOrderDetail);
 
--- backend/src/routes/rating.routes.ts
+++ backend/src/routes/rating.routes.ts
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { createRating, getRating, updateRating, deleteRating } from "../controllers/rating.controllers";
+import { createRating, readRating, updateRating, deleteRating } from "../controllers/rating.controllers";
 
 const router = Router()
 
 router.post("/Rating", createRating);
-router.get("/Rating", getRating);
+router.get("/Rating", readRating);
 router.put("/Rating", updateRating);
 router.delete("/Rating", deleteRating);
 
