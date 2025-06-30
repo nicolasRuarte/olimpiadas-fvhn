058f5bbee9afe968611cc5d8a5bcd10a96845fdf
nicolasRuarte <miqueasnruarte@protonmail.com>
2025-06-29 22:02:53 -0300

    chore: Cambiando nombres por convención

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { createUser, getUser, updateUser, deleteUser } from "../controllers/users.controllers";
+import { createUser, readUser, updateUser, deleteUser } from "../controllers/users.controllers";
 
 const router = Router()
 
 router.post("/users", createUser);
-router.get("/users", getUser);
+router.get("/users", readUser);
 router.put("/users", updateUser);
 router.delete("/users", deleteUser);
 
