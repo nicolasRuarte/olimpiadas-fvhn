ae98e7d750b8b4793c088145d2d1750730e6308f
nicolasRuarte <miqueasnruarte@protonmail.com>
2025-06-29 22:00:18 -0300

    chore: Añadiendo rutas de entidades para futuro uso

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,10 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 import userRoutes from "./routes/user.routes";
+import serviceRoutes from "./routes/service.routes";
+import orderRoutes from "./routes/order.routes";
+import orderDetailRoutes from "./routes/orderdetail.routes";
+import ratingRoutes from "./routes/rating.routes";
 
 const app = express();
 
@@ -10,5 +14,9 @@ app.use(cors());
 app.use(express.json());
 
 app.use(userRoutes);
+app.use(serviceRoutes);
+app.use(orderRoutes);
+app.use(orderDetailRoutes);
+app.use(ratingRoutes);
 
 export default app;
