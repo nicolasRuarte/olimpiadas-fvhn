6687a1e29c1d70edfd5d1c39a6ef34efb501a8a4
nicolasRuarte <miqueasnruarte@protonmail.com>
2025-07-01 19:31:00 -0300

    Agregando validación de datos con Valibot

--- backend/src/controllers/user.controllers.ts
+++ backend/src/controllers/user.controllers.ts
@@ -2,37 +2,43 @@ import { Request, Response } from "express";
 import { User } from "../entities/User";
 import { Order } from "../entities/Order";
 import { AppDataSource } from "../db";
-import { validateStringId } from "../validation";
+import { validateStringId, validateUserData } from "../validation";
+import bcrypt from "bcrypt";
+import { SALT_ROUNDS } from "../config";
 
 
 export async function createUser(req: Request, res: Response) {
-    const { dni, surname, names, email, password, phone_number } = req.body;
+    let loginData = req.body;
 
-    if (!validateStringId(dni)) {
+    if (!validateStringId(loginData.dni)) {
         res.send("Por favor elija un DNI válido");
         return;
     }
-    if (await User.findOne({ where: { dni: dni }}) !== null) {
+    if (await User.findOne({ where: { dni: loginData.dni }}) !== null) {
         res.send("Usuario ya existe");
         return;
     }
 
     const manager = AppDataSource.manager;
 
-    const newOrder = new Order;
-    newOrder.id = dni;
 
     try {
-        await manager.save(newOrder);
+        loginData = validateUserData(loginData);
+        console.log(typeof loginData);
+        console.log(loginData);
         
         const newUser = new User;
-        newUser.dni = dni;
-        newUser.surname = surname;
-        newUser.names = names;
-        newUser.email = email;
-        newUser.password = password;
-        newUser.phone_number = phone_number;
+        newUser.dni = loginData.dni;
+        newUser.surname = loginData.surname;
+        newUser.names = loginData.names;
+        newUser.email = loginData.email;
+        newUser.password = await bcrypt.hash(loginData.password, SALT_ROUNDS);
+        newUser.phone_number = loginData.phone_number;
+
+        const newOrder = new Order;
+        newOrder.id = loginData.dni;
         newUser.order = newOrder;
+        await manager.save(newOrder);
 
         console.log(`Creando usuario ${newUser} con orden ${newOrder}`);
         await manager.save(newUser);
@@ -40,6 +46,7 @@ export async function createUser(req: Request, res: Response) {
         res.send("Usuario creado");
     } catch (error) {
         console.error(error);
+        res.send("Error al crear usuario");
     }
     
 }
--- backend/src/validation.ts
+++ backend/src/validation.ts
@@ -1,3 +1,46 @@
+import * as v from "valibot";
+
+const messages = {
+    string: "debe ser un string",
+    nonEmpty: "debe ingresar el dato",
+    minLength: "longitud debe ser de mínimo 8 caracteres",
+    length: "debe tener longitud de 8 caracteres"
+}
+
+const dniLength = 8;
+const passwordMinLength = 8;
+const phoneNumberLength = 10;
+
+const userLoginSchema = v.object({
+    dni: v.pipe(
+        v.string(messages.string),
+        v.nonEmpty(),
+        v.length(dniLength)
+    ),
+    surname: v.pipe(
+        v.string(messages.string),
+    ),
+    names: v.pipe(
+        v.string(messages.string),
+        v.nonEmpty()
+    ),
+    email: v.pipe(
+        v.string(messages.string),
+        v.email(),
+        v.nonEmpty(messages.nonEmpty)
+    ),
+    password: v.pipe(
+        v.string(messages.string),
+        v.nonEmpty(),
+        v.minLength(passwordMinLength, messages.minLength)
+    ),
+    phone_number: v.pipe(
+        v.string(messages.string),
+        v.nonEmpty(messages.nonEmpty),
+        v.length(phoneNumberLength, messages.length)
+    )
+})
+
 export function validateNumberId(id: number) {
     if (id === undefined || id === null || typeof id !== "number") {
         return false;
@@ -12,4 +55,10 @@ export function validateStringId(id: string) {
     } else {
         return true;
     }
+}
+
+export function validateUserData(loginData: unknown) {
+    type loginData = v.InferOutput<typeof userLoginSchema>;
+
+    return v.parse(userLoginSchema, loginData);
 }
