c9d5130270ba0c804d589f33cf30773d9104e4de
nicolasRuarte <miqueasnruarte@protonmail.com>
2025-07-01 21:47:56 -0300

    Añadiendo validaciones con class-validator

--- backend/src/entities/Order.ts
+++ backend/src/entities/Order.ts
@@ -1,9 +1,13 @@
 import {  Entity, BaseEntity, PrimaryColumn, ManyToMany, JoinTable } from "typeorm";
 import { Service } from "./Service";
+import {
+    IsString
+} from "class-validator";
 
 @Entity()
 export class Order extends BaseEntity {
     @PrimaryColumn()
+    @IsString()
     id: string;
 
     @ManyToMany(() => Service)
