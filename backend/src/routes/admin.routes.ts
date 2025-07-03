a05c82aadba369c7b0cb1b4176b6804a868a7761
nicolasRuarte <miqueasnruarte@protonmail.com>
2025-07-02 23:54:43 -0300

    Pequeño comentario

--- backend/src/routes/admin.routes.ts
+++ backend/src/routes/admin.routes.ts
@@ -5,6 +5,7 @@ const router = Router();
 
 router.get("/admin", readAdmin);
 router.get("/admin/sells", getAllSales);
+// Estatus pueden cambiar de pending a accepted o anulated
 router.patch("/admin/change-order-status", changeOrderDetailStatus);
 
 export default router;
