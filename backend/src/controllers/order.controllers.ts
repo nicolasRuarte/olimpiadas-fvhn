04c8d93ee7af52b5db629abba42827da556b4055
nicolasRuarte <miqueasnruarte@protonmail.com>
2025-07-23 12:11:30 -0300

    Refactorizando para que utilice repositorios en lugar del data manager y para que trabaje con datos envíados por el body en lugar de la URL

--- backend/src/controllers/order.controllers.ts
+++ backend/src/controllers/order.controllers.ts
@@ -1,105 +1,110 @@
 import { Request, Response } from "express";
-import { Service } from "../entities/Service";
+import { Service } from "@entities/Service";
 import { AppDataSource } from "../db";
 import { validateNumberId, validateStringId } from "@functionality/validation";
-import { Order } from "../entities/Order";
+import { Order } from "@entities/Order";
+import { createErrorMessage } from "@functionality/errorMessages";
+import { Item } from "@entities/Item";
+
+const orderRepository = AppDataSource.getRepository(Order);
+const itemRepository = AppDataSource.getRepository(Item);
+const serviceRepository = AppDataSource.getRepository(Service);
+
+export async function readOrderFromServer(id: string) {
+    return await orderRepository.findOne({ where: { id: id }, relations: { items: true }});
+}
 
 export async function readOrder(req: Request, res: Response) {
-    const selectedId = req.params.id;
-
-    if (!validateStringId(selectedId)) {
-        res.status(400).send("Error al leer carrito");
-        return;
-    }
-    
-    const manager = AppDataSource.manager;
-    try {
-        const orderFound = await manager.findOne(Order, { where: { id: selectedId }, relations: { items: true } })
-        if (orderFound === null) throw new Error("La orden solicitada no existe");
-
-        res.status(200).send(orderFound);
-        console.log("Orden leída");
-    } catch (error) {
-        console.error(error);
-        res.status(400).send();
-    }
-
-}
-export async function updateOrderItems(req: Request, res: Response) {
-    const orderId = req.params.id;
-    const serviceId: number = parseInt(req.query.sId as string);
-
-    if (!validateStringId(orderId) || !validateNumberId(serviceId)) {
-        res.status(400).send("Por favor elija un ID de orden o ID de servicio válido");
-        return;
-    }
-
-    const manager = AppDataSource.manager;
+    const { id } = req.body;
 
     try {
-        const order = await manager.findOne(Order, { where: { id: orderId }, relations: { items: true } });
-        if (order === null) throw new Error("ID de orden no existe");
-
-        const serviceToAdd = await manager.findOne(Service, { where: { id: serviceId } });
-        if (serviceToAdd === null) throw new Error("ID de servicio no existe");
-
-        if (order.items === undefined) {
-            order.items = [ serviceToAdd ];
-        } else {
-            order.items.push(serviceToAdd);
+        if (!validateStringId(id)) {
+            throw new Error("invalid-id");
         }
 
-        await manager.save(order)
-        res.status(201).send(order);
-        console.log("Actualizando orden");
+        const orderFound = await readOrderFromServer(id);
+        if (orderFound === null) throw new Error("not-found");
+
+        console.log("Orden leída");
+        res.status(200).send(orderFound);
     } catch (error) {
         console.error(error);
-        res.status(400).send();
-        return;
+        res.send(createErrorMessage(error as string));
     }
+}
+
+async function saveOrderItems(orderId: string, items: Item[]) {
+    const order = await orderRepository.findOne({ where: { id: orderId }, relations: { items: true }});
+    if (order === null) throw new Error("not-found");
+
+    if (order?.items === undefined || order.items === null) {
+        order.items = items;
+    } else {
+        for (const item of items) {
+            // Asigna la bidireccionalidad de las relaciones item-order e item-service
+            itemRepository.save(item);
+
+            order.items.push(item);
+        }
+    }
+    
+    await orderRepository.save(order);
+
+    return order;
+}
+
+export async function updateOrderItems(req: Request, res: Response) {
+    const { orderId, items } = req.body;
+
+    try {
+        const order = await saveOrderItems(orderId, items);
+
+        console.log("Actualizando orden");
+        res.status(201).send(order);
+    } catch (error) {
+        console.error(error);
+        res.status(400).send(createErrorMessage(error as string));
+    }
+}
+
+// Por propósitos de debuggeo, la función devuelve la orden luego de haber borrado sus elementos
+async function deleteItems(id: string, serviceId: number) {
+    const selectAllItemsFlag = -1;
+    const order = await orderRepository.findOne({ where: { id: id }, relations: { items: true } });
+
+    if (order === null) throw new Error("not-found");
+
+    if (order.items === undefined || order.items === null) throw new Error("La orden no tiene ningún elemento");
+    
+    if (serviceId === selectAllItemsFlag) {
+        order.items = [];
+        await orderRepository.save(order);
+        return order;
+    }
+
+    order.items = order.items.filter(function(item) { return item.serviceId !== serviceId });
+
+    await orderRepository.save(order);
+
+    return order;
 }
 
 export async function deleteOrderItems(req: Request, res: Response) {
     const selectAllItemsFlag = -1;
     const orderId = req.params.id;
     const serviceIdToDelete: number = parseInt(req.query.sId as string) || selectAllItemsFlag;
-    if (!validateStringId(orderId) || !validateNumberId(serviceIdToDelete)) {
-        console.error("Por favor elija un ID de orden o ID de servicio válido");
-        res.status(400).send();
-        return;
-    }
 
-    const manager = AppDataSource.manager;
 
     try {
-        let order = await manager.findOne(Order, { where: { id: orderId }, relations: { items: true } });
-        if (order === null) throw new Error("Error de borrado");
+        if (!validateStringId(orderId) || !validateNumberId(serviceIdToDelete)) throw new Error("invalid-id");
 
-        console.log(orderId);
-        if (serviceIdToDelete === selectAllItemsFlag) {
-            // Borra todos los elementos del array
-            order.items = order.items.filter((item) => { return item.id === -1 });
-            await manager.save(order);
-            res.send(order);
-            console.log("Borrados todos los elementos de la orden");
-            return;
-        }
-
-        // item[0] es index e item[1] es el valor
-        const index = 0;
-        const value = 1;
-        for (const entry of order.items.entries()) {
-            if (entry[value].id === serviceIdToDelete) {
-                order.items.splice(entry[index], 1)
-                break;
-            }
-        }
+        const order = await deleteItems(orderId, serviceIdToDelete)
+        if (order === null) throw new Error("not-found");
  
-        await manager.save(order);
-        res.status(204).send();
         console.log("Borrado elemento de orden");
+        res.status(204).send(order);
      } catch (error) {
          console.error(error);
-         res.status(400).send("Error de borrado");
+         res.status(400).send(createErrorMessage(error as string));
      }
 }
