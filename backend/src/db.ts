5d0704a9786e240866bc736d3fee04062146b906
nicolasRuarte <miqueasnruarte@protonmail.com>
2025-07-23 12:19:05 -0300

    fix: Cambiando los paths de las entidades por sus respectivos alias

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,9 +1,10 @@
 import { DataSource } from "typeorm";
-import { User } from "./entities/User";
-import { Order } from "./entities/Order";
-import { OrderDetail } from "./entities/OrderDetail";
-import { Rating } from "./entities/Rating";
-import { Service } from "./entities/Service";
+import { User } from "@entities/User";
+import { Order } from "@entities/Order";
+import { OrderDetail } from "@entities/OrderDetail";
+import { Rating } from "@entities/Rating";
+import { Service } from "@entities/Service";
+import { Item } from "@entities/Item";
 import { DB_NAME, DB_PORT, DB_URL, HOST, PASSWORD, USERNAME } from "./dbconfig";
 
 export const AppDataSource = new DataSource({
@@ -17,7 +18,7 @@ export const AppDataSource = new DataSource({
     password: PASSWORD,
     port: (<number>DB_PORT),
     database: DB_NAME,
-    entities: [ User, Order, OrderDetail, Service, Rating ],
+    entities: [ User, Order, OrderDetail, Service, Rating, Item ],
     logging: true,
-    synchronize: true,
+    synchronize: true
 });
