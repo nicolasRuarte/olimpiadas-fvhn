ea369560f319e20ef087a8580f941e6da2dd5c6f
nicolasRuarte <miqueasnruarte@protonmail.com>
2025-07-24 11:38:11 -0300

    fix: Refactorizando para que utilice repositorios en lugar del DataManager

--- backend/src/controllers/orderdetail.controllers.ts
+++ backend/src/controllers/orderdetail.controllers.ts
@@ -3,7 +3,11 @@ import { AppDataSource } from "../db";
 import { OrderDetail } from "@entities/OrderDetail";
 import { User } from "@entities/User";
 import { validateNumberId, validateStringId } from "@functionality/validation";
-import { deleteOrderItems } from "./order.controllers";
+import { deleteItems, deleteOrderItems } from "./order.controllers";
+import { createErrorMessage } from "@functionality/errorMessages";
+
+const orderDetailRepository = AppDataSource.getRepository(OrderDetail);
+const userRepository  = AppDataSource.getRepository(User);
 
 // Pasando valor any momentáneamente. Cambiar después
 function calculateTotalPrice(items: any) {
@@ -17,25 +21,21 @@ function calculateTotalPrice(items: any) {
 
 export async function createOrderDetail(req: Request, res: Response) {
     const { orderId, items } = req.body; 
-    if (items === undefined || items === null || !validateStringId(orderId)) {
-        console.error("Error en id o productos de la orden");
-        return;
-    }
-    
-
-    const manager = AppDataSource.manager;
 
     try {
+        if (!validateStringId(orderId)) throw new Error("invalid-id");
+        if (items === undefined || items === null) throw new Error("empty-body");
+
         const newOrderDetail = new OrderDetail;
         newOrderDetail.items = items;
         newOrderDetail.total_price = calculateTotalPrice(items);
 
-        await manager.save(newOrderDetail);
+        await orderDetailRepository.save(newOrderDetail);
         console.log("Detalle de orden creado");
 
         // Usamos orderId para sacar la id de usuario porque son la misma
-        const user = await manager.findOne(User, { where: { dni: orderId }, relations: { orderDetails: true } });
-        if ( user === null ) throw new Error("Usuario no existe");
+        const user = await userRepository.findOne({ where: { dni: orderId }, relations: { orderDetails: true } });
+        if ( user === null ) throw new Error("not-found");
 
         if (user.orderDetails === undefined) {
             user.orderDetails = [ newOrderDetail ];
@@ -43,46 +43,41 @@ export async function createOrderDetail(req: Request, res: Response) {
             user.orderDetails.push(newOrderDetail);
         }
 
-        await manager.save(user);
+        await userRepository.save(user);
 
-        req.params.id = orderId;
-        await deleteOrderItems(req, res)
+        const selectAllFlag = -1;
+        await deleteItems(orderId, selectAllFlag);
 
         res.status(201).send(newOrderDetail);
     } catch (error) {
         console.error(error);
+        res.send(createErrorMessage(error as string));
     }
 }
 
 export async function readOrderDetail(req: Request, res: Response) {
     const selectAllFlag = -1;
     const selectedId = parseInt(req.params.id) || selectAllFlag;
-    if (!validateNumberId(selectedId)) {
-        console.log("Error de validación al leer detalle de orden");
-        res.send("Por favor ingrese un ID de detalle de orden válido");
-        return;
-    }
-
-    const dataManager = AppDataSource.manager;
 
     try {
+        if (!validateNumberId(selectedId)) throw new Error("invalid-id");
         let orderDetailFound;
 
         if (selectedId === selectAllFlag) {
-            orderDetailFound = await dataManager.find(OrderDetail, { order: { order_number: "ASC" }});
+            orderDetailFound = await orderDetailRepository.find({ order: { order_number: "ASC" }});
             res.send(orderDetailFound);
             console.log("Devolviendo todos los detalles de orden registrados");
             return;
         }
 
-        orderDetailFound = await dataManager.findOne(OrderDetail, { where: { order_number: selectedId } })
-        if (orderDetailFound === null) throw new Error("El número de orden solicitado no existe");
+        orderDetailFound = await orderDetailRepository.findOne({ where: { order_number: selectedId } })
+        if (orderDetailFound === null) throw new Error("not-found");
 
-        res.status(200).send(orderDetailFound);
         console.log("Devolviendo detalle de orden");
+        res.status(200).send(orderDetailFound);
     } catch (error) {
         console.error(error);
-        res.status(400).send();
+        res.status(400).send(createErrorMessage(error as string));
     }
 }
 
