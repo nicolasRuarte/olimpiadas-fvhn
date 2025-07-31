5362ba6a31e46b77d1fab6a9d65467079a655598
nicolasRuarte <miqueasnruarte@protonmail.com>
2025-07-31 02:22:29 -0300

    chore: Borrando código innecesario

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -12,10 +12,4 @@ router.get("/user", readUsersController);
 router.put("/user", updateUserController);
 router.delete("/user", deleteUserController);
 
-// Obtiene el ID mediante JSON en el body
-//router.get("/user/purchases", getAllPurchases);
-
-//router.get("/user/login", );
-//router.post("/user/login", logInUser)
-
 export default router
