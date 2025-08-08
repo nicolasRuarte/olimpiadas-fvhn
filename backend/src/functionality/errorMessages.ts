0077e2789f5ee661ce3ef2e7b52b5c23076927aa
nicolasRuarte <miqueasnruarte@protonmail.com>
2025-08-08 20:45:05 -0300

    chore: Agregando casos especificos para IDs inválidas Agregando casos para IDs inválidas de tipo string o IDs inválidas de tipo entero

--- backend/src/functionality/errorMessages.ts
+++ backend/src/functionality/errorMessages.ts
@@ -3,6 +3,12 @@ export function createErrorMessage(errorType: Error, statusCode?: number) {
         case "invalid-id":
             return { message: "Por favor ingrese un id válido", statusCode: 400 };
 
+        case "invalid-number-id":
+            return { message: "Error de id numérico no válido", statusCode: 400 };
+
+        case "invalid-string-id":
+            return { message: "Error de id string no válido", statusCode: 400 };
+
         case "not-found":
             return { message: "El objeto solicitado no existe", statusCode: 404 };
         
@@ -10,9 +16,9 @@ export function createErrorMessage(errorType: Error, statusCode?: number) {
             return { message: "Acceso no autorizado. Por favor inicie sesión", statusCode: 401 };
             
         case "empty-body":
-            return { message: "El cuerpo de la request no poseía ningún valor", statusCode: 400 };
+            return { message: "Al cuerpo de la petición le faltaban uno o más datos", statusCode: 400 };
 
         default:
-            return { message: errorType.message, statusCode: statusCode };
+            return { message: errorType.message, statusCode: 500 }; // 0 para indicar que no se sabe qué codigo es
     }
 }
