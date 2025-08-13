383355c391ea0d29811e62a1ef05def99fc0c04a
nicolasRuarte <miqueasnruarte@protonmail.com>
2025-08-13 12:57:18 -0300

    fix: Agregando controlador de cambio de status a rutas orderdetail

--- backend/src/routes/orderdetail.routes.ts
+++ backend/src/routes/orderdetail.routes.ts
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { createOrderDetailController, readOrderDetailController } from "@controllers/orderdetail.controllers";
+import { createOrderDetailController, readOrderDetailController, updateOrderDetailStatusController } from "@controllers/orderdetail.controllers";
 
 const router = Router()
 
 router.post("/orderdetail", createOrderDetailController);
 router.get("/orderdetail", readOrderDetailController); // Devolver todos los OrderDetail solo funciona si pasamos -1 como id
-router.patch("/orderdetail/status", )
+router.patch("/orderdetail/status", updateOrderDetailStatusController)
 
 export default router
