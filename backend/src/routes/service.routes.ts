dd291c8cebd719f546a35f44846e77302054f7c9
nicolasRuarte <miqueasnruarte@protonmail.com>
2025-08-18 22:56:44 -0300

    fix: Agregando middleware de verificación de admin

--- backend/src/routes/service.routes.ts
+++ backend/src/routes/service.routes.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { createServiceController, readServiceControlller, updateServiceController, deleteServiceController } from "@controllers/service.controllers";
+import { verifyIfUserIsAdmin } from "@middlewares/auth.middleware";
 
 const router = Router();
 
-router.post("/service", createServiceController);
+router.post("/service", verifyIfUserIsAdmin, createServiceController);
 router.get("/service", readServiceControlller);
 router.get("/service/:id", readServiceControlller);
-router.patch("/service", updateServiceController);
-router.delete("/service", deleteServiceController);
+router.patch("/service", verifyIfUserIsAdmin, updateServiceController);
+router.delete("/service", verifyIfUserIsAdmin, deleteServiceController);
 
 export default router;
