00b7785fa4a08522081facf8dafac6da0bb33ad0
nicolasRuarte <miqueasnruarte@protonmail.com>
2025-08-31 21:25:36 -0300

    fix: Borrando controlador para crear detalles de órdenes

--- backend/src/routes/orderdetail.routes.ts
+++ backend/src/routes/orderdetail.routes.ts
@@ -1,10 +1,9 @@
 import { Router } from "express";
-import { createOrderDetailController, readOrderDetailController, updateOrderDetailStatusController } from "@controllers/orderdetail.controllers";
+import { readOrderDetailController, updateOrderDetailStatusController } from "@controllers/orderdetail.controllers";
 import { verifyIfUserIsAdmin } from "@middlewares/auth.middleware";
 
 const router = Router()
 
-router.post("/orderdetail", verifyIfUserIsAdmin, createOrderDetailController);
 router.get("/orderdetail", readOrderDetailController); // Devolver todos los OrderDetail solo funciona si pasamos -1 como id
 router.patch("/orderdetail/status", verifyIfUserIsAdmin, updateOrderDetailStatusController);
 
